064fd06f972dc37759905be35ff706b2177e91fd
Clive Gordon <clivejay492@gmail.com>
2018-01-24 00:53:14 +0000

    start structure, add components and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PageComponent} from './page/page.component';
+import {ContentService} from './shared/services/content.service';
+import {AppRoutingModule} from './app-routing.module';
+import { FullpageDirective } from './shared/directives/fullpage.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    FullpageDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ContentService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
