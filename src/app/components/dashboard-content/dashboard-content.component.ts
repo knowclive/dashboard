59d2ecc77bab36571851e7f7dfb5699c915c447c
Clive Gordon <clivejay492@gmail.com>
2018-03-20 09:04:41 +0000

    Fix top rhs component bgd image

--- src/app/components/dashboard-content/dashboard-content.component.ts
+++ src/app/components/dashboard-content/dashboard-content.component.ts
@@ -11,7 +11,6 @@ export class DashboardContentComponent implements OnInit {
     heading1: 'Continue revision for your next assessment',
     heading2: '3 saved bookmarks',
     heading3: 'We launched new qualification',
-    text3: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than.',
     heading4: 'My progress',
     heading5: 'Assessments and results',
     heading6: 'Study support',
